71e2670d9a589b82ef89cab2b934f7681db71c9b
unevilicorn <unevilicorn@gmail.com>
2020-12-01 14:31:48 +0000

    Added dictionary based version of the each method

--- Day 1: Report Repair/solution.py
+++ Day 1: Report Repair/solution.py
@@ -3,23 +3,43 @@ testInput = open(testFileName).readlines()
 
 intInput = list(map(int, testInput))
 
-target = 2020
+finalTarget = 2020
 
 
-def twoEntry():
-    for i in intInput:
-        for j in intInput:
-            if(i + j == target):
-                return(i * j)
+def twoEntry(target=finalTarget, inputs=intInput):
+    for n1 in inputs[:-1]:
+        for n2 in inputs[1:]:
+            if(n1 + n2 == target):
+                return(n1 * n2)
 
 
-def threeEntry():
-    for i in intInput:
-        for j in intInput:
-            for k in intInput:
-                if(i + j + k == target):
-                    return(i * j * k)
+def threeEntry(target=finalTarget, inputs=intInput):
+    for i, n1 in enumerate(inputs[:-2]):
+        r = target - n1
+        tr = twoEntry(r, inputs[i:])
+        if (tr):
+            return tr * n1
+
+
+def twoEntryHash(target=finalTarget, inputs=intInput):
+    h = {}
+    for i in inputs:
+        r = target - i
+        if i in h:
+            return i * r
+        else:
+            h[r] = 0
+
+
+def threeEntryHash(target=finalTarget, inputs=intInput):
+    for i, n1 in enumerate(inputs[:-2]):
+        r = target - n1
+        tr = twoEntryHash(r, inputs[i:])
+        if (tr):
+            return tr * n1
 
 
 print(twoEntry())
+print(twoEntryHash())
 print(threeEntry())
+print(threeEntryHash())
